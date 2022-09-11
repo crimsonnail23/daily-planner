2bca2fc99ff9fe8dc359a7f41575bd9d3d503bbd
night <yseddiq@gmail.com>
2022-09-11 01:08:54 -0400

    got ALL timeblocks to display

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,8 @@ var currentDate = moment().format('MM/DD/YYYY');
 $("#currentDay").text(currentDate);
 
 var hour9=$('<div> 09:00</div>');
-var hour10=$('<div> 10:00</div');
-var hour11=$('<div> 11:00</div');
+var hour10=$('<div> 10:00</div>');
+var hour11=$('<div> 11:00</div>');
 var hour12=$('<div> 12:00</div>');
 var hour13=$('<div> 13:00 </div>');
 var hour14=$('<div> 14:00</div>');
@@ -13,20 +13,9 @@ var hour17=$('<div> 17:00</div>');
 
 
 function task(){
-    $('<p></p>')
-    .addClass("insideInfo")
-    .text("stuff")
+$('<p></p>').text('stuff')
 }
 
-hour9.text(task());
-hour10.text(task());
-hour11.text(task());
-hour12.text(task());
-hour13.text(task());
-hour14.text(task());
-hour15.text(task());
-hour16.text(task());
-hour17.text(task());
 
 hour9.appendTo('.container');
 hour10.appendTo('.container');
